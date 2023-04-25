c1e3950c1cb9f3a9cb326f1febac5658ddcce28c
tensorpro <tensorpro@gmail.com>
2023-04-25 10:18:16 -0700

    cleanup conversion

--- src/transformers/models/rwkv4_neo/convert_rwkv_original_pytorch_checkpoint_to_pytorch.py
+++ src/transformers/models/rwkv4_neo/convert_rwkv_original_pytorch_checkpoint_to_pytorch.py
@@ -31,13 +31,9 @@ from transformers import (
 from transformers.utils import logging
 
 
-FAIRSEQ_MODELS = ["bart.large", "bart.large.mnli",
-                  "bart.large.cnn", "bart_xsum/model.pt"]
-
 logging.set_verbosity_info()
 logger = logging.get_logger(__name__)
 
-SAMPLE_TEXT = " Hello world! cécé herlolip"
 
 rename_keys_dict = {
     'emb.weight': 'model.embeddings.weight',
@@ -57,6 +53,7 @@ key_substring_replacements = [
     ('.ln1', '.time_mix.layer_norm'),
     ('.ln2', '.channel_mix.layer_norm'),
 ]
+
 
 def get_new_key(old_key: str) -> str:
 
@@ -85,106 +82,63 @@ def remove_ignore_keys_(state_dict):
     for k in ignore_keys:
         state_dict.pop(k, None)
 
-def make_linear_from_emb(emb):
-    vocab_size, emb_size = emb.weight.shape
-    lin_layer = nn.Linear(vocab_size, emb_size, bias=False)
-    lin_layer.weight.data = emb.weight.data
-    return lin_layer
-
 
 @torch.no_grad()
-def convert_rwkv_checkpoint(checkpoint_path, pytorch_dump_folder_path, repo, hf_checkpoint_name=None):
+def convert_rwkv_checkpoint(checkpoint_path, pytorch_dump_folder_path,):
     """
-    Copy/paste/tweak model's weights to our BERT structure.
+    Copy/paste/tweak model's weights to our RWKV structure.
     """
-    # if not os.path.exists(checkpoint_path):
-    #     rwkv = torch.hub.load("BlinkDL/fairseq", checkpoint_path).eval()
-    # else:
     state_dict = torch.load(checkpoint_path, 'cpu')
 
-    # rwkv.model.upgrade_state_dict(rwkv.model.state_dict())
-    if hf_checkpoint_name is None:
-        hf_checkpoint_name = checkpoint_path.replace(".", "-")
-    # config = Rwkv4NeoConfig.from_pretrained(hf_checkpoint_name)
-    # tokens = bart.encode(SAMPLE_TEXT).unsqueeze(0)
-    # tokens2 = BartTokenizer.from_pretrained(hf_checkpoint_name).encode(
-    #     SAMPLE_TEXT, return_tensors="pt").unsqueeze(0)
-    # assert torch.eq(tokens, tokens2).all()
-
-    # state_dict = rwkv.state_dict()
     remove_ignore_keys_(state_dict)
     keys = list(state_dict.keys())
     for k in keys:
-
-        if 'emb' in k:
-            # state_dict[k] = state_dict[k]
-            print(state_dict[k])
-
-
-        # if 'time_mix_' in k:
-            # print(k)
-            # state_dict[k] = state_dict[k].squeeze()
-            # print(state_dict[k].shape)
         rename_key(state_dict, k)
 
-    return state_dict
-    # if checkpoint_path == "bart.large.mnli":
-    #     state_dict = rwkv.state_dict()
-    #     remove_ignore_keys_(state_dict)
-    #     state_dict["model.shared.weight"] = state_dict["model.decoder.embed_tokens.weight"]
-    #     for src, dest in mnli_rename_keys:
-    #         rename_key(state_dict, src, dest)
-    #     model = BartForSequenceClassification(config).eval()
-    #     model.load_state_dict(state_dict)
-    #     fairseq_output = bart.predict("mnli", tokens, return_logits=True)
-    #     new_model_outputs = model(tokens)[0]  # logits
-    # else:  # no classification heads to worry about
-    #     state_dict = bart.model.state_dict()
-    #     remove_ignore_keys_(state_dict)
-    #     state_dict["shared.weight"] = state_dict["decoder.embed_tokens.weight"]
-    #     fairseq_output = bart.extract_features(tokens)
-    #     if hf_checkpoint_name == "facebook/bart-large":
-    #         model = BartModel(config).eval()
-    #         model.load_state_dict(state_dict)
-    #         new_model_outputs = model(tokens).model[0]
-    #     else:
-    #         model = BartForConditionalGeneration(
-    #             config).eval()  # an existing summarization ckpt
-    #         model.model.load_state_dict(state_dict)
-    #         if hasattr(model, "lm_head"):
-    #             model.lm_head = make_linear_from_emb(model.model.shared)
-    #         new_model_outputs = model.model(tokens)[0]
-
-    # Check results
-    # assert fairseq_output.shape == new_model_outputs.shape
-    # assert (fairseq_output == new_model_outputs).all().item()
-    # Path(pytorch_dump_folder_path).mkdir(exist_ok=True)
-    # model.save_pretrained(pytorch_dump_folder_path)
+    num_layers = max(int(key.split('.')[2])
+                     for key in state_dict if 'blocks.' in key) + 1
+    vocab_size, hidden_size = state_dict['model.embeddings.weight'].shape
+    cfg = Rwkv4NeoConfig(
+        vocab_size=vocab_size,
+        hidden_size=hidden_size,
+        num_layers=num_layers,
+    )
+    model = Rwkv4NeoForCausalLM(cfg)
+    model.load_state_dict(state_dict)
+    model.save_pretrained(pytorch_dump_folder_path)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Required parameters
     parser.add_argument(
-        "--repo", type=str, help="hugging face repo", default=None,
+        "--hf_repo", type=str, help="path in a file system to an RWKV model",
     )
     parser.add_argument(
-        "path", type=str, help="path in a huggingface space",
+        "--hf_model_path", type=str, help="specific model path in hugginface repo",
     )
-    parser.add_argument("--pytorch_dump_folder_path", default=None,
+    parser.add_argument(
+        "--local_path", type=str, help="path in a file system to an RWKV model",
+    )
+    parser.add_argument("--output_path", default=None,
                         type=str, help="Path to the output PyTorch model.",
                         )
-    parser.add_argument(
-        "--hf_config", default=None, type=str, help="Which huggingface architecture to use: bart-large-xsum",
-    )
     args = parser.parse_args()
-    a = convert_rwkv_checkpoint(
-        args.path, args.pytorch_dump_folder_path, args.repo, hf_checkpoint_name=args.hf_config)
-    print(list(a.keys()))
-    cfg = Rwkv4NeoConfig(vocab_size=50277)
-    m = Rwkv4NeoForCausalLM(cfg)
-    print(m.model.embeddings.state_dict())
-    m.load_state_dict(a)
-    print(m.model.embeddings.state_dict())
-    print(a['model.embeddings.weight'])
-    print(m(torch.arange(10).unsqueeze(0))['logits'][:,-1,:])
+
+    valid_hf = args.hf_repo and args.hf_model_path
+    valid_path = args.local_path
+
+    if not (valid_hf or valid_path):
+        raise ValueError('You must specify either --local_path, or both [--hf_repo_path and --hf_model_path]')
+    
+    if valid_hf and valid_path:
+        raise ValueError('You cannot set both --local_path, and [--hf_repo_path and --hf_model_path].'
+                         'Use --local_path for a checkpoint on your filesystem, and use --hf_repo/--hf_model_path'
+                         'to load a hub model')
+    path = args.local_path
+    print('ayy')
+    if valid_hf:
+        from huggingface_hub import hf_hub_download
+        path = hf_hub_download(args.hf_repo, args.hf_model_path)
+        print(path)
+    convert_rwkv_checkpoint(os.path.expanduser(path), os.path.expanduser(args.output_path))
